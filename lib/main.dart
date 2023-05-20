d4338b8d21aa04976f0a993f2cf126d46fc06d01
AhmedFaragallah <ahmedmahmoudfaragallah@gmail.com>
2023-05-20 04:11:26 +0300

    chane theme and create notes view page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/notes_view.dart';
 
 void main() {
   runApp(const NoteApp());
@@ -9,6 +10,10 @@ class NoteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const NotesView(),
+    );
   }
 }
