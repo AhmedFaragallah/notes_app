29bd717fd940f5239097829e728a5719f701e3ed
AhmedFaragallah <ahmedmahmoudfaragallah@gmail.com>
2023-05-21 03:04:23 +0300

    create custom List View

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
+import 'package:note_app/views/widgets/custom_list_view.dart';
 import 'package:note_app/views/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -15,7 +16,7 @@ class NotesViewBody extends StatelessWidget {
             height: 60,
           ),
           CustomAppBar(),
-          NoteItem(),
+          Expanded(child: CustomListView()),
         ],
       ),
     );
