3372de3343d60290ef175bc4ad3d3b0551efe8cf
AhmedFaragallah <ahmedmahmoudfaragallah@gmail.com>
2023-05-23 03:48:10 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,56 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ListTile(
-                title: const Text(
-                  'Flutter Tips ',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 30,
-                  ),
-                ),
-                subtitle: Padding(
-                  padding: const EdgeInsets.only(top: 16, bottom: 16),
-                  child: Text(
-                    'Bulid Your Career With Ahmed Faragallah ',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ListTile(
+                  title: const Text(
+                    'Flutter Tips ',
                     style: TextStyle(
-                      color: Colors.black.withOpacity(.5),
-                      fontSize: 18,
+                      color: Colors.black,
+                      fontSize: 30,
                     ),
                   ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 16, bottom: 16),
+                    child: Text(
+                      'Bulid Your Career With Ahmed Faragallah ',
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(.5),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  trailing: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.delete,
+                        color: Colors.black,
+                        size: 30,
+                      )),
                 ),
-                trailing: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.delete,
-                      color: Colors.black,
-                      size: 30,
-                    )),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 16),
-                child: Text(
-                  'May 21 , 2022',
-                  style: TextStyle(
-                      color: Colors.black.withOpacity(.5), fontSize: 18),
+                Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: Text(
+                    'May 21 , 2022',
+                    style: TextStyle(
+                        color: Colors.black.withOpacity(.5), fontSize: 18),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ));
+              ],
+            ),
+          )),
+    );
   }
 }
---
+++ lib/views/widgets/edit_note_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class EditNoteViewBody extends StatelessWidget {
+  const EditNoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
